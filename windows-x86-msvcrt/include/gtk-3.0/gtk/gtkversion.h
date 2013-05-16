89a9b514c534e56ea9ac5ff46dbf8bce0bcbb7f1
Dongsheng Song <songdongsheng@live.cn>
2013-05-16 16:41:51 +0800

    Upgrade to gtk+-3.8.2

--- windows-x86-msvcrt/include/gtk-3.0/gtk/gtkversion.h
+++ windows-x86-msvcrt/include/gtk-3.0/gtk/gtkversion.h
@@ -64,7 +64,7 @@
  * application compile time, rather than from the library linked
  * against at application run time.
  */
-#define GTK_MICRO_VERSION (1)
+#define GTK_MICRO_VERSION (2)
 
 /**
  * GTK_BINARY_AGE:
@@ -73,7 +73,7 @@
  * application compile time, rather than from the library linked
  * against at application run time.
  */
-#define GTK_BINARY_AGE    (801)
+#define GTK_BINARY_AGE    (802)
 
 /**
  * GTK_INTERFACE_AGE:
@@ -82,7 +82,7 @@
  * application compile time, rather than from the library linked
  * against at application run time.
  */
-#define GTK_INTERFACE_AGE (1)
+#define GTK_INTERFACE_AGE (2)
 
 /**
  * GTK_CHECK_VERSION:
