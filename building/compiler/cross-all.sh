90296f6b9b51e7071d9316a76eb87b1046c8b08c
Dongsheng Song <songdongsheng@live.cn>
2013-06-14 21:37:49 +0800

    Cleanup

--- building/compiler/cross-all.sh
+++
@@ -1,8 +0,0 @@
-/home/cauchy/cross-win32-dm.sh
-/home/cauchy/cross-win64.sh
-/home/cauchy/cross-arm64.sh
-/home/cauchy/cross-armhf.sh
-/home/cauchy/cross-powerpc64.sh
-/home/cauchy/cross-sparc64.sh
-/home/cauchy/gcc-4.7.x-w32-multilib.sh
-/home/cauchy/gcc-4.7.x-w64-multilib.sh
