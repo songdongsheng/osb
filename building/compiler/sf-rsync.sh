c9d3d3c411d33abceaca7c6a89cd91e5428ed4aa
宋冬生 <dongsheng.song@gmail.com>
2014-09-05 11:05:54 +0800

    Add zlib of msvcr120.

--- building/compiler/sf-rsync.sh
+++ building/compiler/sf-rsync.sh
@@ -3,6 +3,10 @@
 SSH_AUTH_SOCK=/tmp/ssh-TnUEZWzBmFmM/agent.6066; export SSH_AUTH_SOCK;
 SSH_AGENT_PID=6067; export SSH_AGENT_PID;
 
+#
+# https://sourceforge.net/p/forge/documentation/Release Files for Download/
+#
+
 # ssh -t dongsheng,osb@shell.sourceforge.net create
 rsync -tsz --progress -e ssh gcc-4.8-win32_4.8.4-20140824.7z    dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/4.8/x86/testing/
 rsync -tsz --progress -e ssh gcc-4.8-win64_4.8.4-20140824.7z    dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/4.8/x64/testing/
