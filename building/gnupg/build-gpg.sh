0db1bc107bfbbff388b3fa5e73ce1fd692ec0398
Dongsheng Song <dongsheng.song@gmail.com>
2015-09-06 17:59:58 +0800

    Add readme.md

--- building/gnupg/build-gpg.sh
+++ building/gnupg/build-gpg.sh
@@ -2,13 +2,20 @@
 : '
 GnuPG 2.1 depends on the following packages:
 
-ftp://ftp.gnupg.org/GnuPG/libgpg-error/libgpg-error-1.19.tar.bz2
-ftp://ftp.gnupg.org/GnuPG/npth/npth-1.2.tar.bz2
+ftp://ftp.gnupg.org/gcrypt/dirmngr/dirmngr-1.1.1.tar.bz2
+ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.7.tar.bz2
+ftp://ftp.gnupg.org/gcrypt/libassuan/libassuan-2.3.0.tar.bz2
 ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.6.3.tar.bz2
 ftp://ftp.gnupg.org/gcrypt/libksba/libksba-1.3.3.tar.bz2
-ftp://ftp.gnupg.org/gcrypt/libassuan/libassuan-2.2.1.tar.bz2
-ftp://ftp.gnupg.org/gcrypt/pinentry/pinentry-0.9.4.tar.bz2
-ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.5.tar.bz2
+ftp://ftp.gnupg.org/gcrypt/pinentry/pinentry-0.9.4.tar.bz2          [XXX 0.9.5 broken !]
+ftp://ftp.gnupg.org/GnuPG/libgpg-error/libgpg-error-1.20.tar.bz2
+ftp://ftp.gnupg.org/GnuPG/npth/npth-1.2.tar.bz2
+
+http://zlib.net/zlib-1.2.8.tar.gz
+http://ftp.gnu.org/gnu/libiconv/libiconv-1.14.tar.gz
+
+http://download.sourceforge.net/pcre/pcre-8.37.tar.bz2
+http://download.sourceforge.net/pcre/pcre2-10.20.tar.bz2
 '
 #-------------------------------- prepare --------------------------------
 export TARGET_TRIPLET=i686-w64-mingw32
@@ -16,38 +23,40 @@ export TARGET_TRIPLET=x86_64-w64-mingw32
 
 export BUILD_TRIPLET=`/usr/share/misc/config.guess`
 export SYS_ROOT=${HOME}/native/${TARGET_TRIPLET}/gnupg-2.1
-export OBJ_ROOT=${HOME}/obj/x${TARGET_TRIPLET}/gnupg-2.1
+export OBJ_ROOT=${HOME}/obj/${TARGET_TRIPLET}/gnupg-2.1
 
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
-export PATH=${HOME}/cross/i686-windows-gcc-5/bin:${PATH}
-export PATH=${HOME}/cross/x86_64-windows-gcc-5/bin:${PATH}
+export PATH=${HOME}/cross/i686-windows-gcc-4.9/bin:${PATH}
+export PATH=${HOME}/cross/x86_64-windows-gcc-4.9/bin:${PATH}
 export PATH=${SYS_ROOT}/bin:${PATH}
+
+export GNUPG_SRC_ROOT=${OBJ_ROOT}/gnupg-2.1.7
+export LIBASSUAN_SRC_ROOT=${OBJ_ROOT}/libassuan-2.3.0
+export LIBGCRYPT_SRC_ROOT=${OBJ_ROOT}/libgcrypt-1.6.3
+export LIBGCRYPT_SRC_ROOT=/home/cauchy/vcs/git/libgcrypt
+export LIBGPG_ERROR_SRC_ROOT=${OBJ_ROOT}/libgpg-error-1.20
+export LIBICONV_SRC_ROOT=${OBJ_ROOT}/libiconv-1.14
+export LIBKSBA_SRC_ROOT=${OBJ_ROOT}/libksba-1.3.3
+export NPTH_SRC_ROOT=${OBJ_ROOT}/npth-1.2
+export PCRF_SRC_ROOT=${OBJ_ROOT}/pcre-8.37
+export PINENTRY_SRC_ROOT=${OBJ_ROOT}/pinentry-0.9.4
+export PINENTRY_SRC_ROOT=/home/cauchy/vcs/git/pinentry
+export ZLIB_SRC_ROOT=${OBJ_ROOT}/zlib-1.2.8
 
 rm -fr ${SYS_ROOT} ${OBJ_ROOT}
 
 mkdir -p ${OBJ_ROOT} ; cd ${OBJ_ROOT}
 
-tar -xjf ~/sync/building/src/gnupg-2.1.5.tar.bz2
-tar -xjf ~/sync/building/src/libassuan-2.2.1.tar.bz2
+tar -xjf ~/sync/building/src/gnupg-2.1.7.tar.bz2
+tar -xjf ~/sync/building/src/libassuan-2.3.0.tar.bz2
 tar -xjf ~/sync/building/src/libgcrypt-1.6.3.tar.bz2
-tar -xjf ~/sync/building/src/libgpg-error-1.19.tar.bz2
+tar -xjf ~/sync/building/src/libgpg-error-1.20.tar.bz2
 tar -xzf ~/sync/building/src/libiconv-1.14.tar.gz
 tar -xjf ~/sync/building/src/libksba-1.3.3.tar.bz2
 tar -xjf ~/sync/building/src/npth-1.2.tar.bz2
 tar -xjf ~/sync/building/src/pcre-8.37.tar.bz2
 tar -xjf ~/sync/building/src/pinentry-0.9.4.tar.bz2
 tar -xzf ~/sync/building/src/zlib-1.2.8.tar.gz
-
-export GNUPG_SRC_ROOT=${OBJ_ROOT}/gnupg-2.1.5
-export LIBASSUAN_SRC_ROOT=${OBJ_ROOT}/libassuan-2.2.1
-export LIBGCRYPT_SRC_ROOT=${OBJ_ROOT}/libgcrypt-1.6.3
-export LIBGPG_ERROR_SRC_ROOT=${OBJ_ROOT}/libgpg-error-1.19
-export LIBICONV_SRC_ROOT=${OBJ_ROOT}/libiconv-1.14
-export LIBKSBA_SRC_ROOT=${OBJ_ROOT}/libksba-1.3.3
-export NPTH_SRC_ROOT=${OBJ_ROOT}/npth-1.2
-export PCRF_SRC_ROOT=${OBJ_ROOT}/pcre-8.37
-export PINENTRY_SRC_ROOT=${OBJ_ROOT}/pinentry-0.9.4
-export ZLIB_SRC_ROOT=${OBJ_ROOT}/zlib-1.2.8
 
 #-------------------------------- x86_64-w64-mingw32-pkg-config --------------------------------
 mkdir -p ${SYS_ROOT}/bin && cat > ${SYS_ROOT}/bin/${TARGET_TRIPLET}-pkg-config <<EOF
@@ -91,7 +100,7 @@ cd ${PCRF_SRC_ROOT} && ${PCRF_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
     --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET} \
     --disable-cpp --enable-utf --disable-shared --enable-static
 
-make -j8; make install-strip
+make -j8; make install
 
 cat > ${SYS_ROOT}/include/regex.h <<EOF
 #ifndef PCRE_STATIC
@@ -111,45 +120,46 @@ rm -fr ${SYS_ROOT}/lib/regex
 cd ${LIBICONV_SRC_ROOT} && ${LIBICONV_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
     --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET}
 
-make -j8; make install-strip
+make -j8; make install
 
 #-------------------------------- pinentry --------------------------------
 cd ${PINENTRY_SRC_ROOT} && ${PINENTRY_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
-    --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET} \
-    --disable-ncurses --disable-pinentry-tty
+    --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET} --disable-pinentry-tty
 
-make -j8; install -m 0755 -t ${SYS_ROOT}/bin w32/pinentry-w32.exe
+# m4_define([mym4_revision], [1532bf3])
 
-#-------------------------------- libgpg-error --------------------------------
-cd ${LIBGPG_ERROR_SRC_ROOT} && ${LIBGPG_ERROR_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
-    --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET}
-
-make -j8; make install-strip
+make clean; make -j8; make install
 
 #-------------------------------- npth --------------------------------
 cd ${NPTH_SRC_ROOT} && ${NPTH_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
     --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET}
 
-make -j8; make install-strip
+make -j8; make install
+
+#-------------------------------- libgpg-error --------------------------------
+cd ${LIBGPG_ERROR_SRC_ROOT} && ${LIBGPG_ERROR_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
+    --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET}
+
+make -j8; make install
 
 #-------------------------------- libgcrypt --------------------------------
 cd ${LIBGCRYPT_SRC_ROOT} && ${LIBGCRYPT_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
     --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET} \
     --disable-asm --disable-padlock-support
 
-make -j8; make install-strip
+make clean; make -j8; make install
 
 #-------------------------------- libksba --------------------------------
 cd ${LIBKSBA_SRC_ROOT} && ${LIBKSBA_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
     --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET}
 
-make -j8; make install-strip
+make -j8; make install
 
 #-------------------------------- libassuan --------------------------------
 cd ${LIBASSUAN_SRC_ROOT} && ${LIBASSUAN_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
     --build=${BUILD_TRIPLET} --host=${TARGET_TRIPLET}
 
-make -j8; make install-strip
+make -j8; make install
 
 #-------------------------------- gnupg --------------------------------
 cd ${GNUPG_SRC_ROOT} && ${GNUPG_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
@@ -158,35 +168,77 @@ cd ${GNUPG_SRC_ROOT} && ${GNUPG_SRC_ROOT}/configure --prefix=${SYS_ROOT} \
     --disable-g13 --disable-gpgtar \
     --with-zlib=${SYS_ROOT} --with-regex=${SYS_ROOT}
 
-make -j8; make install-strip
+make -j8; make install
 
-${TARGET_TRIPLET}-strip ${SYS_ROOT}/bin/*.dll ${SYS_ROOT}/bin/*.exe ${SYS_ROOT}/libexec/*.exe
+# ${TARGET_TRIPLET}-strip ${SYS_ROOT}/bin/*.dll ${SYS_ROOT}/bin/*.exe ${SYS_ROOT}/libexec/*.exe
 
 : '
-gnupg-2.1/bin/libassuan6-0.dll          libassuan-0.dll
-gnupg-2.1/bin/libgcrypt-20.dll
-gnupg-2.1/bin/libgpg-error6-0.dll       libgpg-error-0.dll
-gnupg-2.1/bin/libiconv-2.dll
-gnupg-2.1/bin/libksba-8.dll
-gnupg-2.1/bin/libnpth6-0.dll            libnpth-0.dll
-gnupg-2.1/bin/zlib1.dll
+-rwxr-xr-x 1 cauchy cauchy  688192 Aug 12 17:09 libassuan6-0.dll
+-rwxr-xr-x 1 cauchy cauchy 4270456 Aug 12 17:09 libgcrypt-20.dll
+-rwxr-xr-x 1 cauchy cauchy  533046 Aug 12 17:09 libgpg-error6-0.dll
+-rwxr-xr-x 1 cauchy cauchy 1538877 Aug 12 17:09 libiconv-2.dll
+-rwxr-xr-x 1 cauchy cauchy 1200416 Aug 12 17:09 libksba-8.dll
+-rwxr-xr-x 1 cauchy cauchy  184690 Aug 12 17:09 libnpth6-0.dll
+-rwxr-xr-x 1 cauchy cauchy  114176 Aug 12 17:09 zlib1.dll
 
-gnupg-2.1/bin/dirmngr.exe
-gnupg-2.1/bin/dirmngr-client.exe
-gnupg-2.1/bin/gpg2.exe                  gpg.exe
-gnupg-2.1/bin/gpg-agent.exe
-gnupg-2.1/bin/gpgconf.exe
-gnupg-2.1/bin/gpg-connect-agent.exe
-gnupg-2.1/bin/gpg-error.exe
-gnupg-2.1/bin/gpgkey2ssh.exe
-gnupg-2.1/bin/gpgsm.exe
-gnupg-2.1/bin/gpgv2.exe                 gpgv.exe
-gnupg-2.1/bin/hmac256.exe
-gnupg-2.1/bin/iconv.exe
-gnupg-2.1/bin/kbxutil.exe
-gnupg-2.1/bin/pinentry-w32.exe          pinentry.exe
+-rwxr-xr-x 1 cauchy cauchy  382583 Aug 12 17:24 dirmngr-client.exe
+-rwxr-xr-x 1 cauchy cauchy 1723647 Aug 12 17:24 dirmngr.exe
+-rwxr-xr-x 1 cauchy cauchy 1618019 Aug 12 17:24 gpg-agent.exe
+-rwxr-xr-x 1 cauchy cauchy  502759 Aug 12 17:24 gpgconf.exe
+-rwxr-xr-x 1 cauchy cauchy  649204 Aug 12 17:24 gpg-connect-agent.exe
+-rwxr-xr-x 1 cauchy cauchy  167571 Aug 12 17:24 gpg-error.exe
+-rwxr-xr-x 1 cauchy cauchy 4254011 Aug 12 17:24 gpg.exe
+-rwxr-xr-x 1 cauchy cauchy 2240886 Aug 12 17:24 gpgsm.exe
+-rwxr-xr-x 1 cauchy cauchy  241406 Aug 12 17:24 hmac256.exe
+-rwxr-xr-x 1 cauchy cauchy  208186 Aug 12 17:24 iconv.exe
+-rwxr-xr-x 1 cauchy cauchy  739264 Aug 12 17:24 kbxutil.exe
+-rwxr-xr-x 1 cauchy cauchy  261844 Aug 12 17:24 pinentry.exe
 
-gnupg-2.1/libexec/gpg-check-pattern.exe
-gnupg-2.1/libexec/gpg-preset-passphrase.exe
-gnupg-2.1/libexec/gpg-protect-tool.exe
+cp  libassuan*.dll libgcrypt*.dll libgpg-error*.dll \
+    libiconv*.dll libksba*.dll libnpth*.dll zlib1.dll \
+    dirmngr.exe dirmngr-client.exe gpg-agent.exe gpgconf.exe \
+    gpg-connect-agent.exe gpg-error.exe gpg2.exe gpgsm.exe \
+    hmac256.exe iconv.exe kbxutil.exe pinentry-w32.exe xxx/
+
+${TARGET_TRIPLET}-strip *.exe *.dll
+
+$ cat > version.txt <<EOF
+gnupg-2.1.7
+
+libgcrypt-1.6.0-256-gfb3cb47
+pinentry-0.9.5-17-g13e0980
+
+dirmngr-1.1.1
+libassuan-2.2.1
+libgpg-error-1.19
+libiconv-1.14
+libksba-1.3.3
+npth-1.2
+pcre-8.37
+zlib-1.2.8
+EOF
+
+7z a -t7z -mx9 -ssc -mtc=on -mmt=on -m0=LZMA2 gnupg-2.1.7-w32-v2.7z gnupg-2.1.7-w32/
+7z a -t7z -mx9 -ssc -mtc=on -mmt=on -m0=LZMA2 gnupg-2.1.7-w64-v2.7z gnupg-2.1.7-w64/
+
+cd %USERPROFILE%\AppData\Roaming\gnupg
+echo Exit Code is %errorlevel%
+
+SET PATH=D:\opt\gnupg-2.1.7-w32
+
+gpg-agent --debug-level expert --daemon
+
+gpg-connect-agent <<EOT
+GETINFO pid
+GETINFO version
+GET_CONFIRMATION Hello
+EOT
+
+gpg --import D:\opt\putty-0.65\ob\0x46D397FF-sec.asc
+gpg --refresh-keys
+gpg --edit-key 46D397FF
+gpg --list-keys
+gpg --list-secret-keys
+gpg -sab .gitconfig
+gpg --verify .gitconfig.asc
 '
---
+++ building/gnupg/readme.md
@@ -0,0 +1,47 @@
+GnuPG 2.1 depends on the following packages:
+
+*) zlib
+=======
+http://zlib.net/zlib-1.2.8.tar.gz
+
+*) pcre (BSD 3-Clause)
+======================
+http://download.sourceforge.net/pcre/pcre-8.37.tar.bz2
+
+*) libiconv (LGPL v2 and GPL v3)
+================================
+http://ftp.gnu.org/gnu/libiconv/libiconv-1.14.tar.gz
+
+*) pinentry (GPL v2)
+====================
+ftp://ftp.gnupg.org/gcrypt/pinentry/pinentry-0.9.4.tar.bz2  [XXX 0.9.5 is broken !]
+
+git://git.gnupg.org/pinentry.git
+http://git.gnupg.org/cgi-bin/gitweb.cgi?p=pinentry.git;a=summary
+
+*) npth (mixed LGPLv3+/GPLv2+)
+==============================
+ftp://ftp.gnupg.org/GnuPG/npth/npth-1.2.tar.bz2
+
+*) libgpg-error (GPL v2)
+========================
+ftp://ftp.gnupg.org/GnuPG/libgpg-error/libgpg-error-1.20.tar.bz2
+
+*) libassuan (LGPLv2.1+/GPLv3+)
+===============================
+ftp://ftp.gnupg.org/gcrypt/libassuan/libassuan-2.3.0.tar.bz2
+
+*) libksba (LGPLv3+/GPLv2+)
+===========================
+ftp://ftp.gnupg.org/gcrypt/libksba/libksba-1.3.3.tar.bz2
+
+*) libgcrypt (LGPLv2.1+/GPLv2+)
+===============================
+ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.6.3.tar.bz2
+
+git://git.gnupg.org/libgcrypt.git
+http://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgcrypt.git;a=summary
+
+*) GnuPG (mixed LGPLv3+/GPLv2+)
+===============================
+ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.7.tar.bz2
