4d9f1029135b2433acaf13aad8c04a14015b5929
宋冬生 <songdongsheng@live.cn>
2016-01-07 17:41:14 +0800

    Add mpfr.txt

--- building/compiler/sf-rsync.sh
+++ building/compiler/sf-rsync.sh
@@ -9,34 +9,28 @@
 
 cd /home/cauchy/native
 
-SSH_AUTH_SOCK=/tmp/ssh-X8slUywvAlXN/agent.13185; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=13186; export SSH_AGENT_PID;
+SSH_AUTH_SOCK=/tmp/ssh-6BcDSTKZu9Nk/agent.16804; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=16805; export SSH_AGENT_PID;
 
 # rsync -tsz -e ssh README-w64.rst  dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win32/Personal Builds'/dongsheng-daily/README.rst
 # rsync -tsz -e ssh README-w64.rst  dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win64/Personal Builds'/dongsheng-daily/README.rst
 
 rsync -tsz -e ssh README.rst    dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/
 
-rsync -tsz --progress -e ssh gcc-4.8-win32_4.8.5-20150415.7z    dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/4.8/x86/testing/
-rsync -tsz --progress -e ssh gcc-4.8-win64_4.8.5-20150415.7z    dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/4.8/x64/testing/
+rsync -tsz --progress -e ssh gcc-4.9-win32_4.9.4-20160106.7z    dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/4.9/x86/testing/
+rsync -tsz --progress -e ssh gcc-4.9-win64_4.9.4-20160106.7z    dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/4.9/x64/testing/
 
-rsync -tsz --progress -e ssh gcc-4.8-win32_4.8.5-20150415.7z    dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win32/Personal Builds'/dongsheng-daily/4.8/
-rsync -tsz --progress -e ssh gcc-4.8-win64_4.8.5-20150415.7z    dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win64/Personal Builds'/dongsheng-daily/4.8/
+rsync -tsz --progress -e ssh gcc-4.9-win32_4.9.4-20160106.7z    dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win32/Personal Builds'/dongsheng-daily/4.9/
+rsync -tsz --progress -e ssh gcc-4.9-win64_4.9.4-20160106.7z    dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win64/Personal Builds'/dongsheng-daily/4.9/
 
-rsync -tsz --progress -e ssh gcc-4.9-win32_4.9.3-20150415.7z    dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/4.9/x86/testing/
-rsync -tsz --progress -e ssh gcc-4.9-win64_4.9.3-20150415.7z    dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/4.9/x64/testing/
+rsync -tsz --progress -e ssh gcc-5-win32_5.3.1-20160106.7z      dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/5.x/x86/testing/
+rsync -tsz --progress -e ssh gcc-5-win64_5.3.1-20160106.7z      dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/5.x/x64/testing/
 
-rsync -tsz --progress -e ssh gcc-4.9-win32_4.9.3-20150415.7z    dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win32/Personal Builds'/dongsheng-daily/4.9/
-rsync -tsz --progress -e ssh gcc-4.9-win64_4.9.3-20150415.7z    dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win64/Personal Builds'/dongsheng-daily/4.9/
+rsync -tsz --progress -e ssh gcc-5-win32_5.3.1-20160106.7z      dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win32/Personal Builds'/dongsheng-daily/5.x/
+rsync -tsz --progress -e ssh gcc-5-win64_5.3.1-20160106.7z      dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win64/Personal Builds'/dongsheng-daily/5.x/
 
-rsync -tsz --progress -e ssh gcc-5-win32_5.0.1-20150415.7z      dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/5.x/x86/testing/
-rsync -tsz --progress -e ssh gcc-5-win64_5.0.1-20150415.7z      dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/5.x/x64/testing/
+rsync -tsz --progress -e ssh gcc-6-win32_6.0.0-20160106.7z      dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/6.x/x86/testing/
+rsync -tsz --progress -e ssh gcc-6-win64_6.0.0-20160106.7z      dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/6.x/x64/testing/
 
-rsync -tsz --progress -e ssh gcc-5-win32_5.0.1-20150415.7z      dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win32/Personal Builds'/dongsheng-daily/5.x/
-rsync -tsz --progress -e ssh gcc-5-win64_5.0.1-20150415.7z      dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win64/Personal Builds'/dongsheng-daily/5.x/
-
-rsync -tsz --progress -e ssh gcc-6-win32_6.0.0-20150415.7z      dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/6.x/x86/testing/
-rsync -tsz --progress -e ssh gcc-6-win64_6.0.0-20150415.7z      dongsheng,osb@frs.sourceforge.net:/home/frs/project/osb/gcc/6.x/x64/testing/
-
-rsync -tsz --progress -e ssh gcc-6-win32_6.0.0-20150415.7z      dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win32/Personal Builds'/dongsheng-daily/6.x/
-rsync -tsz --progress -e ssh gcc-6-win64_6.0.0-20150415.7z      dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win64/Personal Builds'/dongsheng-daily/6.x/
+rsync -tsz --progress -e ssh gcc-6-win32_6.0.0-20160106.7z      dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win32/Personal Builds'/dongsheng-daily/6.x/
+rsync -tsz --progress -e ssh gcc-6-win64_6.0.0-20160106.7z      dongsheng,mingw-w64@frs.sourceforge.net:/home/frs/project/mingw-w64/'Toolchains targetting Win64/Personal Builds'/dongsheng-daily/6.x/
--- building/compiler/sf-w64-README.rst
+++ building/compiler/sf-w64-README.rst
@@ -1,23 +1,23 @@
 GCC 6 (experimental)
-=======================
+====================
 * `gcc-6-win32 <http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/dongsheng-daily/6.x/>`_
 * `gcc-6-win64 <http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/dongsheng-daily/6.x/>`_
 
 
-GCC 5
-=======================
+GCC 5 (stable)
+==============
 * `gcc-5-win32 <http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/dongsheng-daily/5.x/>`_
 * `gcc-5-win64 <http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/dongsheng-daily/5.x/>`_
 
 
-GCC 4.9
-=======
+GCC 4.9 (stable)
+================
 * `gcc-4.9-win32 <http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/dongsheng-daily/4.9/>`_
 * `gcc-4.9-win64 <http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/dongsheng-daily/4.9/>`_
 
 
-GCC 4.8
-=======
+GCC 4.8 (retired)
+=================
 * `gcc-4.8-win32 <http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/dongsheng-daily/4.8/>`_
 * `gcc-4.8-win64 <http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/dongsheng-daily/4.8/>`_
 
